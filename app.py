fcf834a1a6ab03ff33b05bc5a3516d366daab315
ikialo <synarcapp@gmail.com>
2022-05-05 00:27:51 +1000

    jinja for loops to display info

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 cred_obj = firebase_admin.credentials.Certificate('serviceAcc.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
-	'databaseURL':''
+	'databaseURL':'https://firecorn-default-rtdb.asia-southeast1.firebasedatabase.app/'
 	})
 
 
@@ -21,3 +25,13 @@ for x,y in games.items():
         print(n['Name'])
 
     print("")
+
+
+
+
+@app.route('/')
+def index():
+
+    game = games
+
+    return render_template('index.html', games=game)
